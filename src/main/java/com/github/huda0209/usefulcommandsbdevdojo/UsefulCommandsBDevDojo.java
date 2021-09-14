cd1b906d08548026715064c003566e4d6a905029
huda0209 <65341393+huda0209@users.noreply.github.com>
2021-09-14 23:38:39 +0900

    [fix] implements CommandExecutorがない問題を修正

--- src/main/java/com/github/huda0209/usefulcommandsbdevdojo/UsefulCommandsBDevDojo.java
+++ src/main/java/com/github/huda0209/usefulcommandsbdevdojo/UsefulCommandsBDevDojo.java
@@ -8,7 +8,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class UsefulCommandsBDevDojo extends JavaPlugin {
+public final class UsefulCommandsBDevDojo extends JavaPlugin implements CommandExecutor{
 
     @Override
     public void onEnable() {
